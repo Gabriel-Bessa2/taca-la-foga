46a8781a370b23cdb9e74b195dcfadcfddbe3bb4
Gabriel-Bessa2 <ghfbessa@gmail.com>
2023-12-19 20:27:05 -0300

    improved enemy behavior

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -6,27 +6,34 @@ extends CharacterBody2D
 @export var dmg: int = 1
 @export var speed: int = 50
 var moneyMultiplier: int = 1
-var isAttacking: bool = false
 
-enum ENEMY_STATES {ALIVE, FOLLOWING, DEAD, HURT, LOAD_ATTACK, ATTACKING}
+#Attacking
+@onready var attackTimer: Timer = $AttackTimer
+var isAttacking: bool = false
+var canAttack: bool = true
+
+#state machine
+enum ENEMY_STATES {IDLE, FOLLOWING, DEAD, HURT, LOAD_ATTACK, ATTACKING}
 var current_state: ENEMY_STATES
 
+#movement
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 @export var target: Player
-
 @export var sight_range: float = 100
 @export var attack_range: float = 30
 
-@onready var timer: Timer = $Timer
+#immunity
+@onready var immunityTimer: Timer = $ImmunityTimer
+var immune: bool = false
 
 func _ready():
-	current_state = ENEMY_STATES.ALIVE
+	current_state = ENEMY_STATES.IDLE
 	navigation_agent.path_desired_distance = attack_range
 	navigation_agent.target_desired_distance = attack_range
 
-func _process(delta):
+func _process(_delta):
 	match current_state:
-		ENEMY_STATES.ALIVE:
+		ENEMY_STATES.IDLE:
 			pass
 		ENEMY_STATES.FOLLOWING:
 			pass
@@ -37,22 +44,34 @@ func _process(delta):
 		ENEMY_STATES.ATTACKING:
 			pass
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	var player_distance = position.distance_to(target.position)
+	
 	match current_state:
-		ENEMY_STATES.ALIVE:
-			if(position.distance_to(target.position) < sight_range):
-				current_state = ENEMY_STATES.FOLLOWING
+		ENEMY_STATES.IDLE:
+			velocity = Vector2.ZERO
+			
+			if(player_distance < sight_range and player_distance <= attack_range and canAttack):
+				change_state(ENEMY_STATES.ATTACKING)
+			elif(player_distance < sight_range):
+				change_state(ENEMY_STATES.FOLLOWING)
+		
 		ENEMY_STATES.FOLLOWING:
 			handle_navigation()
+			
+			if(player_distance < sight_range and player_distance <= attack_range and canAttack):
+				change_state(ENEMY_STATES.ATTACKING)
+			elif(player_distance < sight_range and player_distance <= attack_range):
+				change_state(ENEMY_STATES.IDLE)
 		ENEMY_STATES.DEAD:
-			pass
+			velocity = Vector2.ZERO
+			
 		ENEMY_STATES.HURT:
 			pass
-		ENEMY_STATES.LOAD_ATTACK:
-			pass
 		ENEMY_STATES.ATTACKING:
-			#print("Attack")
-			current_state = ENEMY_STATES.FOLLOWING
+			pass
+	
+	move_and_slide()
 
 func handle_navigation() -> void:
 	call_deferred("actor_setup")
@@ -61,12 +80,7 @@ func handle_navigation() -> void:
 		var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
 		velocity = current_agent_position.direction_to(next_path_position) * speed
-		look_at(target.position)
-		
-		move_and_slide()
-	elif (navigation_agent.is_target_reached()):
-		current_state = ENEMY_STATES.LOAD_ATTACK
-		timer.start()
+		aim_at_player()
 
 func actor_setup():
 	await get_tree().physics_frame
@@ -74,10 +88,31 @@ func actor_setup():
 
 func set_movement_target(movement_target: Vector2):
 	navigation_agent.target_position = movement_target
+	
+func end_attack():
+	canAttack = false
+	isAttacking = false
+	attackTimer.start()
+	current_state = ENEMY_STATES.IDLE
+
+func change_state(stateToChange: ENEMY_STATES):
+	current_state = stateToChange
+
+func aim_at_player():
+	look_at(target.position)
+
+func attack_cooldown_out():
+	canAttack = true
+
+func immunity_timeout():
+	immune = false
 
 func damage(damageTaken: int):
 	health = max(health - damageTaken, 0)
+	immune = true
+	immunityTimer.start()
+	
 	if health == 0:
-		current_state = ENEMY_STATES.DEAD
+		change_state(ENEMY_STATES.DEAD)
 	else:
-		current_state = ENEMY_STATES.HURT
+		change_state(ENEMY_STATES.HURT)
